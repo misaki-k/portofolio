2eb04342ff7f8f5638837ea16d636d4f8b47915c
misaki-k <misamisa.chocolate@gmail.com>
2018-01-19 15:49:40 +0900

    Update Connectdb.java

--- cosmeticsapp/src/main/java/com/cosmeticsapp/springboot/Connectdb.java
+++ cosmeticsapp/src/main/java/com/cosmeticsapp/springboot/Connectdb.java
@@ -7,84 +7,187 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
-
 public class Connectdb {
-	
+
 	// データベース接続と結果取得のための変数
 	private Connection con = null;
 	private Statement stmt = null;
 	private ResultSet rs = null;
-	
+
 	public void dbaccess() {
-			//JDBCドライバの読み込み
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				//JDBCドライバが見つからない場合、例外
-				e.printStackTrace();
+		// JDBCドライバの読み込み
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// JDBCドライバが見つからない場合、例外
+			e.printStackTrace();
+		}
+		try {
+			// DBへの接続を行う
+			con = DriverManager.getConnection("jdbc:mysql://localhost/cosmetics", "root", "password");
+			// DB処理のための準備、Statementオブジェクトの作成
+			stmt = con.createStatement();
+		} catch (SQLException e) {
+			// DBとの処理で何らかのエラーがあった場合の例外
+			e.printStackTrace();
+		}
+	}
+
+	// Skintoneテーブルよりデータ取得
+	public ArrayList<SkintoneData> getSkintone() {
+
+		try {
+			// Select文の実行と結果を格納/代入
+			rs = stmt.executeQuery("SELECT * FROM skintone;");
+			ArrayList<SkintoneData> dataList = new ArrayList<SkintoneData>();
+
+			while (rs.next()) {
+				SkintoneData sd = new SkintoneData();
+
+				sd.setId(rs.getString("tone_id"));
+				sd.setVal(rs.getString("toneName"));
+				sd.setRgb(rs.getString("rgbColor"));
+
+				dataList.add(sd);
 			}
+			return dataList;
+		} catch (SQLException e) {
+			// DBとの処理で何らかのエラーがあった場合の例外
+			e.printStackTrace();
+		}
+		// データ取得失敗の場合はNULLを返す
+		return null;
+	}
+	
+	//Skintypeテーブルよりデータ取得
+	public ArrayList<SkintypeData> getSkintype() {
+
+		try {
+			// Select文の実行と結果を格納/代入
+			rs = stmt.executeQuery("SELECT * FROM skintype;");
+			ArrayList<SkintypeData> dataList = new ArrayList<SkintypeData>();
+
+			while (rs.next()) {
+				SkintypeData st = new SkintypeData();
+
+				st.setId(rs.getString("type_id"));
+				st.setVal(rs.getString("typeName"));
+				dataList.add(st);
+			}
+			return dataList;
+		} catch (SQLException e) {
+			// DBとの処理で何らかのエラーがあった場合の例外
+			e.printStackTrace();
+		}
+		// データ取得失敗の場合はNULLを返す
+		return null;
+	}
+	
+	//Coverage
+	public ArrayList<CoverageData> getCoverage(){
+		try {
+			rs = stmt.executeQuery("Select * FROM coverage");
+			ArrayList<CoverageData> dataList = new ArrayList<CoverageData>();
+			
+			while(rs.next()) {
+				CoverageData cd = new CoverageData();
+				
+				cd.setId(rs.getString("coverage_id"));
+				cd.setVal(rs.getString("coverageName"));
+				dataList.add(cd);
+			}
+			
+			return dataList;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	//Finish
+	public ArrayList<FinishData> getFinish(){
+		try {
+			rs = stmt.executeQuery("Select * FROM finish");
+			ArrayList<FinishData> dataList = new ArrayList<FinishData>();
+			
+			while(rs.next()) {
+				FinishData fd = new FinishData();
+				
+				fd.setId(rs.getString("finish_id"));
+				fd.setVal(rs.getString("finishName"));
+				dataList.add(fd);
+			}
+			return dataList;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public ArrayList<CosmeData> getData(Data data) {
+		// JDBCドライバの読み込み
+		try {			
+			String sql =	"SELECT * FROM cosmetics.items WHERE item_id IN " +
+							"(	SELECT " +
+							"		item_id " +
+							"	FROM " +
+							"		(SELECT item_id FROM itemskintone WHERE tone_id = " + data.getAnswer1() + ") as tone" +
+							"		INNER JOIN " +
+							"		(SELECT item_id FROM itemskintype WHERE type_id = " + data.getAnswer2() + ") as type USING (item_id)" +
+							"		INNER JOIN " +
+							"		(SELECT item_id FROM itemcoverage WHERE cover_id = " + data.getAnswer3() + ") as cover USING (item_id)" +
+							"		INNER JOIN " +
+							"		(SELECT item_id FROM itemfinish WHERE finish_id = " + data.getAnswer4() + ") as finish USING (item_id)" +
+							");";
+			// FOR DEBUG
+			System.out.println(sql);
+			rs = stmt.executeQuery(sql);
+			
+			// 結果を表示する
+			ArrayList<CosmeData> dataList = new ArrayList<CosmeData>();
+
+			while (rs.next()) {
+				CosmeData cd = new CosmeData();
+
+				cd.setName(rs.getString("name"));
+				cd.setBrand(rs.getString("brand"));
+				cd.setColor(rs.getString("color"));
+				cd.setPicture(rs.getString("picture"));
+
+				dataList.add(cd);
+			}
+			
+			return dataList;
+		} catch (SQLException e) {
+			// DBとの処理で何らかのエラーがあった場合の例外
+			e.printStackTrace();
+		}
+		// データ取得失敗の場合はNULLを返す
+		return null;
+	}
+
+	// DB終了処理
+	public void close() {
+		if (rs != null) {
 			try {
-				//DBへの接続を行う
-				con = DriverManager.getConnection("jdbc:mysql://localhost/cosmetics","root","password");
-				//DB処理のための準備、Statementオブジェクトの作成
-				stmt = con.createStatement();				
+				rs.close();
 			} catch (SQLException e) {
-				//DBとの処理で何らかのエラーがあった場合の例外
 				e.printStackTrace();
-			} 
+			}
 		}
-		
-		public ArrayList<CosmeData> getData(Data data){
-			//JDBCドライバの読み込み
+		if (stmt != null) {
 			try {
-				//Select文の実行と結果を格納/代入
-				rs = stmt.executeQuery("select items.item_id,items.name,items.brand,items.color,items.picture from itemfinish, finish,(select items.item_id,items.name,items.brand,items.color,items.picture from itemskintype,skintype, (select items.item_id,items.name,items.brand,items.color,items.picture from coverage,itemcoverage,(select items.item_id,items.name,items.brand,items.color,items.picture from items,skintone,itemskintone where skintone.tone_id = itemskintone.tone_id and items.item_id = itemskintone.item_id and skintone.toneName = '" + data.getAnswer1() + "') as items where items.item_id = itemcoverage.item_id and itemcoverage.cover_id = coverage.cover_id and coverage.coverName = '" + data.getAnswer3() + "') as items where items.item_id = itemskintype.item_id and itemskintype.type_id = skintype.type_id and skintype.typeName = '" + data.getAnswer2() + "') as items where items.item_id = itemfinish.item_id and itemfinish.finish_id = finish.finish_id and finish.finishName = '" + data.getAnswer4() + "'");
-				//結果を表示する
-				ArrayList<CosmeData> dataList = new ArrayList<CosmeData>();
-				
-				while(rs.next()) {
-					CosmeData cd = new CosmeData();
-					
-					cd.setName(rs.getString("name"));
-					cd.setBrand(rs.getString("brand"));
-					cd.setColor(rs.getString("color"));
-					cd.setPicture(rs.getString("picture"));
-					
-					
-					dataList.add(cd);
-				}
-				return dataList;
-			}catch(SQLException e) {
-				//DBとの処理で何らかのエラーがあった場合の例外
+				stmt.close();
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			//データ取得失敗の場合はNULLを返す
-			return null;
 		}
-		//DB終了処理
-		public void close() {
-				if (rs != null) {
-					try {
-						rs.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				if (stmt != null) {
-					try {
-						stmt.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				if (con != null) {
-					try {
-						con.close();
-					} catch (SQLException e){
-						e.printStackTrace();
-					}
-				
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
+		}
 	}
 }
